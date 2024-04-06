098c4fe611791d39e4079a85557dc6ad0860de47
Alfonso Gonzalez <info@wsemantic.com>
2024-04-06 22:13:10 +0200

    Update models.py

--- models/models.py
+++ models/models.py
@@ -24,34 +24,40 @@ class ProductTemplate(models.Model):
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    #attribute_serie_id = fields.Many2one('attribute.serie', string='Attribute Serie', related='product_id.attribute_serie_id')
+    def _open_variant_grid_wizard(self):
+        _logger.info("WSEM crear ventana")
+        wizard = self.env['variant.grid.wizard'].create({
+            'purchase_order_line_id': self.id,
+            'attribute_serie_id': self.attribute_serie_id.id,
+        })
+        return {
+            'name': 'Cuadrícula de Variantes',
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'variant.grid.wizard',
+            'res_id': wizard.id,
+            'target': 'new',
+            'context': self.env.context,
+        }
 
     @api.onchange('product_id')
     def onchange_product_id(self):
         _logger.info("WSEM onchange_product_id")
         if self.product_id:
             self.attribute_serie_id = self.product_id.attribute_serie_id
-            # Lógica para abrir el wizard con datos pre-cargados
-            wizard = self.env['variant.grid.wizard'].create({
-                'purchase_order_line_id': self.id,
-                'attribute_serie_id': self.attribute_serie_id.id,
-                # Aquí deberías pre-cargar los nombres de las tallas y cualquier otra información necesaria
-            })
-            return {
-                'name': 'Cuadrícula de Variantes',
-                'type': 'ir.actions.act_window',
-                'view_mode': 'form',
-                'res_model': 'variant.grid.wizard',
-                'res_id': wizard.id,
-                'target': 'new',
-                'context': self.env.context,
-            }
+            return self._open_variant_grid_wizard()
+        else:
+            self.attribute_serie_id = False
 
+    @api.model
     def create(self, vals):
-        if vals.get('product_id'):
-            product_id = self.env['product.product'].browse(vals['product_id'])
-            vals['attribute_serie_id'] = product_id.attribute_serie_id.id
-        return super(PurchaseOrderLine, self).create(vals)
+        _logger.info("WSEM create product")
+        line = super(PurchaseOrderLine, self).create(vals)
+        if line.product_id:
+            line.attribute_serie_id = line.product_id.attribute_serie_id
+            # Llamar al método onchange_product_id después de asignar attribute_serie_id
+            line.onchange_product_id()
+        return line
         
     def create_variant_lines(self):
         variant_grid = self.env.context.get('variant_grid', {})
@@ -79,12 +85,9 @@ class VariantGridWizardCell(models.TransientModel):
 
     wizard_id = fields.Many2one('variant.grid.wizard', string="Wizard de Variantes")
     color_id = fields.Many2one('product.attribute.value', string="Color")
-    talla_1 = fields.Char("Talla 1")
-    talla_2 = fields.Char("Talla 2")
-    talla_3 = fields.Char("Talla 3")
-
-    # Asume que estos campos son para mostrar los nombres de tallas y no para editar cantidades.
-    # Las cantidades se gestionarían en otros campos o en lógica adicional.
+    talla_1 = fields.Char("Talla 1", force_save=True)
+    talla_2 = fields.Char("Talla 2", force_save=True)
+    talla_3 = fields.Char("Talla 3", force_save=True)
 
 class VariantGridWizard(models.TransientModel):
     _name = 'variant.grid.wizard'
@@ -97,13 +100,19 @@ class VariantGridWizard(models.TransientModel):
     def onchange_attribute_serie_id(self):
         details = []
         if self.attribute_serie_id:
-            tallas = self.attribute_serie_id.talla_ids[:3]  # Asume que este campo devuelve las tallas relevantes.
+            tallas = self.attribute_serie_id.item_ids.mapped('attribute_value_id')
+            num_tallas = len(tallas)
+            
             # Crear o actualizar la "fila de encabezado" con los nombres de las tallas.
-            details.append((0, 0, {
+            header_vals = {
                 'color_id': False,  # No hay color para la fila de encabezados.
-                'talla_1': tallas[0].name if len(tallas) > 0 else "",
-                'talla_2': tallas[1].name if len(tallas) > 1 else "",
-                'talla_3': tallas[2].name if len(tallas) > 2 else "",
-            }))
+            }
+            for i in range(3):
+                if i < num_tallas:
+                    header_vals[f'talla_{i+1}'] = tallas[i].name
+                else:
+                    header_vals[f'talla_{i+1}'] = False
+            details.append((0, 0, header_vals))
+            
             # Añade el resto de tus detalles aquí.
         self.detail_ids = details
