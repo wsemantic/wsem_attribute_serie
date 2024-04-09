623042eee2fddd701bb5ae8efda57f15632da3ce
Alfonso Gonzalez <info@wsemantic.com>
2024-04-09 09:39:27 +0200

    v12

--- static/src/js/variant_grid_wizard.js
+++ static/src/js/variant_grid_wizard.js
@@ -1,26 +1,22 @@
 odoo.define('wsem_attribute_serie.CustomFieldMany2One', function(require) {
     'use strict';
 
-    var core = require('web.core');
-    var relational_fields = require('web.relational_fields');
-    var FieldMany2One = relational_fields.FieldMany2One;
+    var FieldMany2One = require('web.relational_fields').FieldMany2One;
     var fieldRegistry = require('web.field_registry');
 
     var CustomFieldMany2One = FieldMany2One.extend({
-        // Sobreescribe el método que se dispara después de que un valor es seleccionado
-        _setValue: function (value, options) {
-            // Primero, ejecuta la lógica original para asegurarte de que el valor se establece correctamente
-			console.log("Set value llamado")
-            return this._super.apply(this, arguments).then(() => {
-                // Después de establecer el valor, dispara un evento personalizado
-                // con el valor seleccionado (puede ser el ID de la serie seleccionada)
-                this.trigger_up('attribute_serie_changed', {serieId: value});
-            });
+        // Intenta usar _onChange si _setValue no está siendo llamado
+        _onChange: function() {
+            // Asegúrate de llamar a la implementación original para no perder funcionalidad
+            this._super.apply(this, arguments);
+            console.log("El campo attribute_serie_id ha cambiado.");
+            // Aquí tu lógica adicional...
         },
     });
 
     fieldRegistry.add('custom_field_many2one_attribute_serie', CustomFieldMany2One);
 });
+
 
 odoo.define('wsem_attribute_serie.CustomFormController', function (require) {
     'use strict';
