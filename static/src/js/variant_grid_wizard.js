a97b4d8d9ac0d7ad295c8bce1d4e2e577839b1af
Alfonso Gonzalez <info@wsemantic.com>
2024-04-09 13:01:44 +0200

    Update variant_grid_wizard.js

--- static/src/js/variant_grid_wizard.js
+++ static/src/js/variant_grid_wizard.js
@@ -10,50 +10,44 @@ odoo.define('variant_grid_wizard.form', function (require) {
             field_changed: '_onFieldChanged',
         }),
 
+        // Este método maneja el evento field_changed
         _onFieldChanged: function (event) {
             var changes = event.data.changes || {};
+            // Verifica si el cambio ocurrió en attribute_serie_id
             if ('attribute_serie_id' in changes) {
-                // No es necesario llamar a reload aquí si la actualización de los encabezados
-                // se maneja a través de _updateTableHeader, que ya maneja la carga.
+                // Llama a _updateTableHeader para actualizar los encabezados
                 this._updateTableHeader();
             }
+            // Asegúrate de llamar al método super para no interrumpir la cadena de eventos
             this._super.apply(this, arguments);
         },
 
+        // Actualiza los encabezados de las columnas basándose en los valores de nombres_tallas
         _updateTableHeader: function() {
             var self = this;
-            // Al remover reload, asegurarse de que los cambios en nombres_tallas se reflejen
-            // directamente sin recargar todo el formulario.
+            // Obtiene el registro actual para acceder a los datos del modelo
             var record = self.model.get(self.handle, {raw: true});
+            // Parsea los nombres de tallas desde el campo nombres_tallas
             var nombresTallas = JSON.parse(record.data.nombres_tallas || "[]");
 
             console.log("Nombres de Tallas:", nombresTallas);
 
+            // Actualiza los textos de los encabezados en el DOM
             $('table.o_list_table thead tr th[data-name^="talla_"]').each(function(index) {
-                // Se actualiza el texto de cada cabecera de columna con los nombres de las tallas
-                $(this).text(nombresTallas[index] || '');
+                if (index < nombresTallas.length) {
+                    $(this).text(nombresTallas[index] || '');
+                }
             });
         },
-
-        // Se sobreescribe el método saveRecord para manejar la actualización de las cabeceras
-        // después de añadir una nueva línea, ya que este evento también podría requerir recargar
-        // los nombres de tallas.
-        saveRecord: function() {
-            var self = this;
-            // Llama primero al método saveRecord original para asegurar que se guarden los cambios.
-            return this._super.apply(this, arguments).then(function () {
-                // Después de guardar, actualiza las cabeceras para reflejar cualquier cambio.
-                self._updateTableHeader();
-            });
-        },
-
     });
 
+    // Extendemos FormView para utilizar nuestro FormController personalizado
     var VariantGridFormView = FormView.extend({
         config: _.extend({}, FormView.prototype.config, {
             Controller: VariantGridFormController,
         }),
     });
 
+    // Registramos nuestra vista personalizada para que esté disponible en Odoo
     viewRegistry.add('variant_grid_wizard_form', VariantGridFormView);
 });
