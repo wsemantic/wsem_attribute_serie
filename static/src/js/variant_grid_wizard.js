fe21f9e7df4436edb72df72c81349a7d32568ed8
Alfonso Gonzalez <info@wsemantic.com>
2024-04-09 13:17:42 +0200

    Update variant_grid_wizard.js

--- static/src/js/variant_grid_wizard.js
+++ static/src/js/variant_grid_wizard.js
@@ -8,52 +8,36 @@ odoo.define('variant_grid_wizard.form', function (require) {
     var VariantGridFormController = FormController.extend({
         custom_events: _.extend({}, FormController.prototype.custom_events, {
             field_changed: '_onFieldChanged',
-            // Considera añadir otros eventos si son necesarios para tu lógica
         }),
 
         _onFieldChanged: function (event) {
+            var self = this;
             var changes = event.data.changes || {};
-            if ('attribute_serie_id' in changes) {
-                // No es necesario llamar a reload aquí si la actualización de los encabezados
-                // se maneja a través de _updateTableHeader, que ya maneja la carga.
-                this._updateTableHeader();
+            // Verifica si el cambio ocurrió en 'attribute_serie_id' o se añadieron líneas
+            if ('attribute_serie_id' in changes || 'line_ids' in changes) {
+                // Aquí incorporamos la llamada a reload antes de actualizar la cabecera
+                this.reload().then(function() {
+                    // Una vez recargado, actualiza las cabeceras.
+                    self._updateTableHeader();
+                });
             }
             this._super.apply(this, arguments);
         },
 
-        // Asegúrate de llamar a _updateTableHeader después de cualquier acción que requiera una actualización
         _updateTableHeader: function() {
+            // Esta función se mantiene como estaba, actualizando las cabeceras de la tabla
             var self = this;
-            // Obtiene el registro actual para acceder a los datos del modelo
             var record = self.model.get(self.handle, {raw: true});
-            // Parsea los nombres de tallas desde el campo nombres_tallas
             var nombresTallas = JSON.parse(record.data.nombres_tallas || "[]");
 
             console.log("Nombres de Tallas:", nombresTallas);
 
-            // Actualiza los textos de los encabezados en el DOM
             $('table.o_list_table thead tr th[data-name^="talla_"]').each(function(index) {
                 if (index < nombresTallas.length) {
                     $(this).text(nombresTallas[index] || '');
                 }
             });
         },
-
-        // Reincorporando reload con un control más refinado
-        reload: function() {
-            var self = this;
-            return this._super.apply(this, arguments).then(function() {
-                self._updateTableHeader(); // Llama a actualizar los encabezados después de recargar
-            });
-        },
-
-        // Un ejemplo de cómo podrías manejar el evento de añadir una nueva línea.
-        // Esto dependerá de cómo estés añadiendo nuevas líneas en tu interfaz
-        _onAddLine: function() {
-            // Posiblemente necesites una lógica aquí para manejar específicamente la adición de nuevas líneas
-            this.reload(); // Esto recargará y luego actualizará los encabezados
-        },
-
     });
 
     var VariantGridFormView = FormView.extend({
