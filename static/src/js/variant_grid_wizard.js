8e3fa784354cfc267f8d934d50769579589922d9
Alfonso Gonzalez <info@wsemantic.com>
2024-04-09 12:56:03 +0200

    Update variant_grid_wizard.js

--- static/src/js/variant_grid_wizard.js
+++ static/src/js/variant_grid_wizard.js
@@ -13,28 +13,39 @@ odoo.define('variant_grid_wizard.form', function (require) {
         _onFieldChanged: function (event) {
             var changes = event.data.changes || {};
             if ('attribute_serie_id' in changes) {
+                // No es necesario llamar a reload aquí si la actualización de los encabezados
+                // se maneja a través de _updateTableHeader, que ya maneja la carga.
                 this._updateTableHeader();
             }
             this._super.apply(this, arguments);
         },
 
-		_updateTableHeader: function() {
-			var self = this;
-			//this.model.reload(this.handle).then(function() {
-				var record = self.model.get(self.handle, {raw: true});
-				// Parsea el valor de nombres_tallas desde la cadena JSON a un array de JavaScript
-				
-				var nombresTallas = JSON.parse(record.data.nombres_tallas || "[]");
-				
-				console.log("Nombres de Tallas:", nombresTallas);
-											
-				
-				$('table.o_list_table thead tr th[data-name^="talla_"]').each(function(index) {
-					// Se actualiza el texto de cada cabecera de columna con los nombres de las tallas
-					$(this).text(nombresTallas[index] || '');
-				});
-			//});
-		},
+        _updateTableHeader: function() {
+            var self = this;
+            // Al remover reload, asegurarse de que los cambios en nombres_tallas se reflejen
+            // directamente sin recargar todo el formulario.
+            var record = self.model.get(self.handle, {raw: true});
+            var nombresTallas = JSON.parse(record.data.nombres_tallas || "[]");
+
+            console.log("Nombres de Tallas:", nombresTallas);
+
+            $('table.o_list_table thead tr th[data-name^="talla_"]').each(function(index) {
+                // Se actualiza el texto de cada cabecera de columna con los nombres de las tallas
+                $(this).text(nombresTallas[index] || '');
+            });
+        },
+
+        // Se sobreescribe el método saveRecord para manejar la actualización de las cabeceras
+        // después de añadir una nueva línea, ya que este evento también podría requerir recargar
+        // los nombres de tallas.
+        saveRecord: function() {
+            var self = this;
+            // Llama primero al método saveRecord original para asegurar que se guarden los cambios.
+            return this._super.apply(this, arguments).then(function () {
+                // Después de guardar, actualiza las cabeceras para reflejar cualquier cambio.
+                self._updateTableHeader();
+            });
+        },
 
     });
 
