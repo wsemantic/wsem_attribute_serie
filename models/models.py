24694c9cdb5f655c53b0bb25d2d09ea0695f18b3
Alfonso Gonzalez <info@wsemantic.com>
2024-04-25 10:53:38 +0200

    Update models.py

--- models/models.py
+++ models/models.py
@@ -17,16 +17,17 @@ class AttributeSerieItem(models.Model):
     attribute_serie_id = fields.Many2one('attribute.serie', string='Attribute Serie', required=True, ondelete='cascade')
     attribute_value_id = fields.Many2one('product.attribute.value', string='Attribute Value', required=True)
 
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    attribute_serie_id = fields.Many2one('attribute.serie', string='Attribute Serie')
-
-    @api.onchange('attribute_serie_id')
-    def _onchange_attribute_serie_id(self):
-        if self.attribute_serie_id:
-            attribute_id = self.env['product.attribute'].search([('name', '=', 'Talla')], limit=1)
-            if attribute_id:
+@api.onchange('attribute_serie_id')
+def _onchange_attribute_serie_id(self):
+    if self.attribute_serie_id:
+        attribute_id = self.env['product.attribute'].search([('name', '=', 'Talla')], limit=1)
+        if attribute_id:
+            existing_line = self.attribute_line_ids.filtered(lambda line: line.attribute_id == attribute_id)
+            if existing_line:
+                _logger.info(f"WSEM existia linea serie")
+                existing_line.value_ids = [(6, 0, self.attribute_serie_id.item_ids.mapped('attribute_value_id').ids)]
+            else:
+                _logger.info(f"WSEM creando serie")
                 self.attribute_line_ids = [(0, 0, {
                     'attribute_id': attribute_id.id,
                     'value_ids': [(6, 0, self.attribute_serie_id.item_ids.mapped('attribute_value_id').ids)]
