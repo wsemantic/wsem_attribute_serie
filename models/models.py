70437b614b1a9cf8c6d3a1ffd0141fb0cb1ddba4
Alfonso Gonzalez <info@wsemantic.com>
2024-04-25 13:00:01 +0200

    v3

--- models/models.py
+++ models/models.py
@@ -24,20 +24,27 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     attribute_serie_id = fields.Many2one('attribute.serie', string='Attribute Serie')
-    
+
     @api.onchange('attribute_serie_id')
     def _onchange_attribute_serie_id(self):
         if self.attribute_serie_id:
             attribute_id = self.env['product.attribute'].search([('name', '=', 'Talla')], limit=1)
             if attribute_id:
                 existing_line = self.attribute_line_ids.filtered(lambda line: line.attribute_id == attribute_id)
+
+                # Obtener los valores de atributo ordenados por secuencia y nombre
+                sorted_attribute_values = self.attribute_serie_id.item_ids.sorted(key=lambda item: (item.sequence or 0, item.attribute_value_id.name))
+                attribute_value_ids = sorted_attribute_values.mapped('attribute_value_id').ids
+
                 if existing_line:
                     _logger.info(f"WSEM existia linea serie")
-                    existing_line.value_ids = [(6, 0, self.attribute_serie_id.item_ids.mapped('attribute_value_id').ids)]
+                    existing_line.value_ids = [(6, 0, attribute_value_ids)]
                 else:
                     _logger.info(f"WSEM creando serie")
                     self.attribute_line_ids = [(0, 0, {
                         'attribute_id': attribute_id.id,
-                        'value_ids': [(6, 0, self.attribute_serie_id.item_ids.mapped('attribute_value_id').ids)]
+                        'value_ids': [(6, 0, attribute_value_ids)]
                     })]
     
+    
+ 
