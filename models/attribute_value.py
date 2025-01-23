fd986980333351356f8fcb80a338e4ebe21051d4
Alfonso Gonzalez <info@wsemantic.com>
2025-01-23 11:43:16 +0100

    no redefinir code de attribute value

--- models/attribute_value.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class ProductAttributeValue(models.Model):
-    _inherit = 'product.attribute.value'
-
-    code = fields.Char(string='Code', help='Unique code for the attribute value')
