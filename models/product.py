b2d5fa4d198c906d40afd1b55f294ed48c746403
Alfonso Gonzalez <info@wsemantic.com>
2025-02-07 10:38:07 +0100

    Update product.py

--- models/product.py
+++ models/product.py
@@ -1,5 +1,8 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
+import json
+import logging
+_logger = logging.getLogger(__name__)
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
