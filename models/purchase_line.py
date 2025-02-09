ed5c429d3a563fa6d31ad94b0ae49ee1496fd155
Alfonso Gonzalez <info@wsemantic.com>
2025-02-09 18:24:32 +0100

    coste

--- models/purchase_line.py
+++ models/purchase_line.py
@@ -1,12 +1,16 @@
 from odoo import models, api
+import logging
+_logger = logging.getLogger(__name__)
 
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
     @api.onchange('price_unit')
     def _onchange_price_unit_propagate(self):
+        _logger.info(f"WSEM compras cambio precio")
         # Se ejecuta cuando se asigna un precio distinto de 0
         if self.product_id and self.price_unit and self.price_unit != 0:
+            _logger.info(f"WSEM existe precio")
             # Filtra las líneas del mismo pedido que:
             #  - Pertenecen al mismo product.template (es decir, son variantes del mismo producto)
             #  - Tienen price_unit igual a 0 (no han sido modificadas)
@@ -16,4 +20,5 @@ class PurchaseOrderLine(models.Model):
                              (not line.price_unit or line.price_unit == 0)
             )
             if similar_lines:
+                _logger.info(f"WSEM linea similar")
                 similar_lines.price_unit = self.price_unit
